6b7dba84018be09be3f9dbb8be14aba6aed6142f
Gustavo <gustavo.moya@geniusreferrals.com>
2023-03-02 10:48:13 -0500

    adding method to test notification

--- api/src/web/web.controller.ts
+++ api/src/web/web.controller.ts
@@ -6,6 +6,9 @@ import { diskStorage } from 'multer';
 import { editFileName } from './../utils/file-upload';
 import { WebService } from './web.service';
 import { Web } from './web.entity';
+import {NotificationService} from "../base/services/notification.service";
+import {NotificationDto} from "../base/dto/notification.dto";
+import {UserService} from "../user/user.service";
 
 @Controller('webs')
 export class WebController {
@@ -13,6 +16,8 @@ export class WebController {
     private webService: WebService,
     @InjectQueue('domainDuplicates')
     private readonly domainDuplicatesQueue: Queue,
+    private readonly userService: UserService,
+    private readonly notificationService: NotificationService
   ) {}
 
   private readonly logger = new Logger(WebController.name);
@@ -22,6 +27,24 @@ export class WebController {
     const webs = await this.webService.findAll();
 
     return webs;
+  }
+
+  @Get('/message')
+  async sendMessage(): Promise<boolean> {
+    const user = await this.userService.getByUuid("ec814ae2-7d1e-4641-9a6c-1d227207cdad");
+    const id = Number(user.id);
+    const notification: NotificationDto = {
+      title: "Dominios duplicados",
+      text: "Proceso terminado con exito.",
+      to: [id],
+      store: false,
+      btnText: "Ver Detalles",
+      type: "success",
+    };
+
+    await this.notificationService.send(notification);
+
+    return true;
   }
 
   @Post('/duplicates')
